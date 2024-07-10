14614a5c73c0634060bd0d111ebf59cae493f244
vikakvasova <vika.kvasova@yahoo.com>
2024-07-10 16:17:02 +0300

    fix ls

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -1,27 +1,29 @@
-export function getPinterest() {
+function getPinterest() {
     return localStorage.getItem('gallery');
 }
-export function setPinterest(array) {
+function setPinterest(array) {
     return localStorage.setItem('gallery', JSON.stringify(array));
 }
 
-export function getDeskOne() {
+function getDeskOne() {
     return localStorage.getItem('deskOne');
 }
-export function setDeskOne(array) {
+function setDeskOne(array) {
     return localStorage.setItem('deskOne', JSON.stringify(array));
 }
 
-export function getDeskTwo() {
+function getDeskTwo() {
     return localStorage.getItem('deskTwo');
 }
-export function setDeskTwo(array) {
+function setDeskTwo(array) {
     return localStorage.setItem('deskTwo', JSON.stringify(array));
 }
 
-export function getDeskThree() {
+function getDeskThree() {
     return localStorage.getItem('deskThree');
 }
-export function setDeskThree(array) {
+function setDeskThree(array) {
     return localStorage.setItem('deskThree', JSON.stringify(array));
 }
+
+export{getPinterest, setPinterest, getDeskOne, setDeskOne, getDeskTwo, setDeskTwo, getDeskThree, setDeskThree}
